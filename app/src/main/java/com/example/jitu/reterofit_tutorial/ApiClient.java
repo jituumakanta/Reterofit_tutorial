f256ccbc92d734d45060469d1cfe4cbd66d4ee59
jituumakanta <jitu.umakanta@gmail.com>
2017-05-08 01:21:48 +0530

    Initial commit

---
+++ app/src/main/java/com/example/jitu/reterofit_tutorial/ApiClient.java
@@ -0,0 +1,26 @@
+package com.example.jitu.reterofit_tutorial;
+
+/**
+ * Created by jitu on 5/8/2017.
+ */
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+
+public class ApiClient {
+
+    public static final String BASE_URL = "http://api.themoviedb.org/3/";
+    private static Retrofit retrofit = null;
+
+
+    public static Retrofit getClient() {
+        if (retrofit==null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
---
+++ app/src/main/java/com/example/jitu/reterofit_tutorial/ApiInterface.java
@@ -0,0 +1,21 @@
+package com.example.jitu.reterofit_tutorial;
+
+/**
+ * Created by jitu on 5/8/2017.
+ */
+
+
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+
+public interface ApiInterface {
+    @GET("movie/top_rated")
+    Call<MoviesResponse> getTopRatedMovies(@Query("api_key") String apiKey);
+
+    @GET("movie/{id}")
+    Call<MoviesResponse> getMovieDetails(@Path("id") int id, @Query("api_key") String apiKey);
+}
